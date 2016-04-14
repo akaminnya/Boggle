0c9f6fb638d87534599862cb76d96ecd39b6861e
unicab <tamnhinho1989@gmail.com>
2016-04-13 23:53:58 -0600

    fafafafafe

--- TruckPairingVideoLink/app/src/main/java/com/daimler/videolink/truckpairingvideolink/MainActivity.java
+++ TruckPairingVideoLink/app/src/main/java/com/daimler/videolink/truckpairingvideolink/MainActivity.java
@@ -18,7 +18,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Get the layout from video_main.xml
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.stream);
 
         // Locate the button in menu_main.xml
         button = (Button) findViewById(R.id.MyButton);
